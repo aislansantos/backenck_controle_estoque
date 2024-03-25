85f7a0f1862e4ed7ba8bb0b888c5f57c14844ffb
Aislan Santos <aislan.santos@gmail.com>
2024-03-25 15:55:06 -0300

    instalação do cors e uso no server, uso do path para configuração do static da pasta public, termino do model de customer, uso do urlencoded extend, e retorno de 404 se não for encontrado a URL.

--- src/models/Customer.ts
+++ src/models/Customer.ts
@@ -2,45 +2,54 @@ import { Model, DataTypes } from "sequelize";
 
 import sequelize from "../instances/pg";
 
-export interface CustomerInstace extends Model {
+export interface ICustomer extends Model {
     id: number;
     name: string;
     email: string;
     city: string;
 }
 
-export const Customer = sequelize.define<CustomerInstace>("Customer", {
-    id: {
-        primaryKey: true,
-        autoIncrement: true,
-        type: DataTypes.INTEGER,
-    },
-    name: {
-        type: DataTypes.STRING,
-        set(value: string) {
-            if (value === undefined || value === null) {
-                return false;
-            }
-            return this.setDataValue("name", value.toUpperCase());
+export const Customer = sequelize.define<ICustomer>(
+    "Customer",
+    {
+        id: {
+            primaryKey: true,
+            autoIncrement: true,
+            type: DataTypes.INTEGER,
+        },
+        name: {
+            type: DataTypes.STRING,
+            set(value: string) {
+                if (value === undefined || value === null) {
+                    return false;
+                }
+                return this.setDataValue("name", value.toUpperCase());
+            },
+        },
+        email: {
+            type: DataTypes.STRING,
+            set(value: string) {
+                if (value === undefined || value === null) {
+                    return false;
+                }
+                return this.setDataValue("email", value.toLowerCase());
+            },
+        },
+        city: {
+            type: DataTypes.STRING,
+            set(value: string) {
+                if (value === undefined || value === null) {
+                    return false;
+                }
+                return this.setDataValue(
+                    "city",
+                    value.charAt(0).toUpperCase() + value.slice(1)
+                );
+            },
         },
     },
-    email: {
-        type: DataTypes.STRING,
-        set(value: string) {
-            if (value === undefined || value === null) {
-                return false;
-            }
-            return this.setDataValue("email", value.toLowerCase());
-        },
-    },
-    city: {
-        type: DataTypes.STRING,
-        set(value: string) {
-            if (value === undefined || value === null) {
-                return false;
-            }
-            const startCity = value.charAt();
-        },
+    {
+        tableName: "customer",
+        timestamps: false,
     }
-},
-});
+);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
+import cors from "cors";
 import dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response, urlencoded } from "express";
+import path from "path";
 
 import routes from "./routes/routes";
 
 dotenv.config();
 const server = express();
 
+server.use(cors());
+
+server.use(express.static(path.join(__dirname, "../public")));
+
+server.use(urlencoded({ extended: true }));
+
 server.use(routes);
+
+server.use((req: Request, res: Response) => {
+    res.status(404).json({ error: "Endpoint não encontrado." });
+});
 
 server.listen(process.env.PORT);
